d3a119392797adcd54309fb8c537c8e0ceef59c6
Sanjay.R <133894028+Sanjayr4@users.noreply.github.com>
2025-04-10 19:52:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-
 import './App.css';
 import Home from './pages/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import View from './pages/view';
 import Order from './pages/order';
 import ScrollToTop from "./ScrollToTop";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 function App() {
   return (
     <>
       <div className="app">
-
-        <BrowserRouter>
-
-        <ScrollToTop />
+        <HashRouter>
+          <ScrollToTop />
           <Routes>
-            <Route path = '/' element = {<Home />} />
-            <Route path = '/view/:id' element = {<View />} />
-            <Route path = '/order/:id' element = {<Order/>}/>
-            
+            <Route path='/' element={<Home />} />
+            <Route path='/view/:id' element={<View />} />
+            <Route path='/order/:id' element={<Order />} />
           </Routes>
           <ToastContainer position="top-right" autoClose={3000} />
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </>
   );
 }
 
 export default App;
-
